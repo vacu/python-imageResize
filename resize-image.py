7b6778936508040f9df599a9c26a31a71062d07e
Calin Stefanescu <c.stefanescu@bloopark.de>
2012-11-27 22:22:06 +0100

    Added crop image

--- resize-image.py
+++ resize-image.py
@@ -1,24 +1,38 @@
 import sys, os, Image, ExifTags
 
 # image file
-imageFile = Image.open(sys.argv[1])
+imageFile = Image.open(sys.argv[1]);
 
-if imageFile.size[0] > imageFile.size[1]:
-  imageFormat = 'landscape'
+imageX = imageFile.size[0] / 2;
+imageY = imageFile.size[1] / 2;
+
+
+if len(sys.argv) > 3:
+  size = sys.argv[3];
+  box = (imageX - (int(size) / 2), imageY - (int(size) / 2), imageX + int(size), imageY + int(size));
+  region = imageFile.crop(box);
+
+  region.save(
+    os.path.splitext(sys.argv[1])[0] + '.thumb' + os.path.splitext(sys.argv[1])[1],
+    imageFile.format
+  )
 else:
-  imageFormat = 'portrait'
+  if imageFile.size[0] > imageFile.size[1]:
+    imageFormat = 'landscape';
+  else:
+    imageFormat = 'portrait';
 
-if imageFormat == 'landscape':
-  if sys.argv[2] < imageFile.size[1]:
-    if imageFile.format is 'JPEG':
-      imageFile = imageFile.rotate(180, expand=True)
+  if imageFormat == 'landscape':
+    if sys.argv[2] < imageFile.size[1]:
+      if imageFile.format is 'JPEG':
+        imageFile = imageFile.rotate(180, expand=True);
 
-  imageFile.thumbnail((int(sys.argv[2]), int(imageFile.size[1])), Image.ANTIALIAS)
-else:
-  imageFile.thumbnail((int(imageFile.size[0]), int(sys.argv[2])), Image.ANTIALIAS)
+    imageFile.thumbnail((int(sys.argv[2]), int(imageFile.size[1])), Image.ANTIALIAS);
+  else:
+    imageFile.thumbnail((int(imageFile.size[0]), int(sys.argv[2])), Image.ANTIALIAS);
 
-imageFile.save(
-  os.path.splitext(sys.argv[1])[0] + '.thumb' + os.path.splitext(sys.argv[1])[1],
-  imageFile.format,
-  quality=100
-)
+  imageFile.save(
+    os.path.splitext(sys.argv[1])[0] + '.thumb' + os.path.splitext(sys.argv[1])[1],
+    imageFile.format,
+    quality=100
+  )
