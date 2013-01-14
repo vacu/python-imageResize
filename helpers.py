7148c49989dfd738bec8528c4144f8064b00c532
Calin Stefanescu <c.stefanescu@bloopark.de>
2013-01-14 12:25:14 +0100

    Added image helpers, completed folder thumb creation

--- helpers.py
+++ helpers.py
@@ -1,2 +1,52 @@
-def createThumb(imageFile, newFile, fileFormat):
-  return imageFile.save(newFile, fileFormat, quality=100);
+import sys, os, Image, ExifTags
+
+class ImageHelper:
+  def __init__(self):
+    self.data = [];
+
+  def createThumb(self, imageFile):
+    # landscape
+    if imageFile.size[0] > imageFile.size[1]:
+      if sys.argv[3] < imageFile.size[1]:
+        if imageFile.format is 'JPEG':
+          imageFile = imageFile.rotate(180, expand=True);
+
+      imageFile.thumbnail((int(sys.argv[3]), int(imageFile.size[1])), Image.ANTIALIAS);
+    # portrait
+    else:
+      imageFile.thumbnail((int(imageFile.size[0]), int(sys.argv[3])), Image.ANTIALIAS);
+
+  # imageType can be image (by default) or the filename in case we are passing the folder
+  def saveFile(self, imageFile, imageType = 'image'):
+    if imageType == 'image':
+      if len(sys.argv) > 4:
+        # if the 4th param is not null then append it to the filename
+        if sys.argv[4] is not None:
+          imageFile.save(
+            os.path.splitext(sys.argv[2])[0] + '.thumb.' + sys.argv[4] + os.path.splitext(sys.argv[2])[1],
+            imageFile.format,
+            quality=100
+          );
+      # if there are not more than 4 params than create the thumb
+      else:
+        imageFile.save(
+          os.path.splitext(sys.argv[2])[0] + '.thumb' + os.path.splitext(sys.argv[2])[1],
+          imageFile.format,
+          quality=100
+        );
+    else:
+      if len(sys.argv) > 4:
+        # if the 4th param is not null then append it to the filename
+        if sys.argv[4] is not None:
+          imageFile.save(
+            sys.argv[2] + os.path.splitext(imageType)[0] + '.thumb.' + sys.argv[4] + os.path.splitext(imageType)[1],
+            imageFile.format,
+            quality=100
+          );
+      # if there are not more than 4 params than create the thumb
+      else:
+        imageFile.save(
+          sys.argv[2] + os.path.splitext(imageType)[0] + '.thumb' + os.path.splitext(imageType)[1],
+          imageFile.format,
+          quality=100
+        );
--- resize-image.py
+++ resize-image.py
@@ -1,41 +1,29 @@
 import sys, os, Image, ExifTags
+# custom functions
+import helpers
 
-# image file
-if sys.argv[1] != '-test':
-  imageFile = Image.open(sys.argv[2]);
-  imageX = imageFile.size[0] / 2;
-  imageY = imageFile.size[1] / 2;
+# instantiate the class
+imgHelper = helpers.ImageHelper();
 
 if len(sys.argv) > 1:
-  # THUMB
-  if sys.argv[1] == '-thumb':
-    # landscape
-    if imageFile.size[0] > imageFile.size[1]:
-      if sys.argv[3] < imageFile.size[1]:
-        if imageFile.format is 'JPEG':
-          imageFile = imageFile.rotate(180, expand=True);
+  # image file
+  if sys.argv[1] != '-folder':
+    imageFile = Image.open(sys.argv[2]);
+    imageX = imageFile.size[0] / 2;
+    imageY = imageFile.size[1] / 2;
 
-      imageFile.thumbnail((int(sys.argv[3]), int(imageFile.size[1])), Image.ANTIALIAS);
-    # portrait
-    else:
-      imageFile.thumbnail((int(imageFile.size[0]), int(sys.argv[3])), Image.ANTIALIAS);
-
-    # check if we have more than 4 params
-    if len(sys.argv) > 4:
-      # if the 4th param is not null then append it to the filename
-      if sys.argv[4] is not None:
-        imageFile.save(
-          os.path.splitext(sys.argv[2])[0] + '.thumb.' + sys.argv[4] + os.path.splitext(sys.argv[2])[1],
-          imageFile.format,
-          quality=100
-        );
-    # if there are not more than 4 params than create the thumb
-    else:
-      imageFile.save(
-        os.path.splitext(sys.argv[2])[0] + '.thumb' + os.path.splitext(sys.argv[2])[1],
-        imageFile.format,
-        quality=100
-      );
+  # SINGLE FILE
+  if sys.argv[1] == '-image':
+    imgHelper.createThumb(imageFile);
+    imgHelper.saveFile(imageFile);
+  # FOLDER
+  elif sys.argv[1] == '-folder':
+    for root, dirs, files in os.walk(sys.argv[2]):
+      for file in files:
+        if file.endswith('.jpg') or file.endswith('.png'):
+          imageFile = Image.open(sys.argv[2] + file);
+          imgHelper.createThumb(imageFile);
+          imgHelper.saveFile(imageFile, file);
   # CROP
   elif sys.argv[1] == '-crop':
     size = sys.argv[3];
@@ -46,24 +34,3 @@ if len(sys.argv) > 1:
       os.path.splitext(sys.argv[2])[0] + '.thumb' + os.path.splitext(sys.argv[2])[1],
       imageFile.format
     );
-  elif sys.argv[1] == '-test':
-    for root, dirs, files in os.walk(sys.argv[2]):
-      for file in files:
-        if file.endswith('.jpg') or file.endswith('.png'):
-          imageFile = Image.open(sys.argv[2] + file);
-
-          if imageFile.size[0] > imageFile.size[1]:
-            if sys.argv[3] < imageFile.size[1]:
-              if imageFile.format is 'JPEG':
-                imageFile = imageFile.rotate(180, expand=True);
-
-            imageFile.thumbnail((int(sys.argv[3]), int(imageFile.size[1])), Image.ANTIALIAS);
-          # portrait
-          else:
-            imageFile.thumbnail((int(imageFile.size[0]), int(sys.argv[3])), Image.ANTIALIAS);
-
-          imageFile.save(
-            sys.argv[2] + os.path.splitext(file)[0] + '.thumb' + os.path.splitext(file)[1],
-            imageFile.format,
-            quality=100
-          );
