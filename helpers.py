7e2ff9bd0e2f0c259af77118f866b500bdc67f5e
Calin Stefanescu <c.stefanescu@bloopark.de>
2013-01-21 13:58:56 +0100

    Single image thumb creation & ration fix

--- helpers.py
+++ helpers.py
@@ -1,36 +1,38 @@
-import sys, os, Image, ExifTags
+import sys, os, Image
 
 class ImageHelper:
   def __init__(self):
     self.data = [];
 
-  def createThumb(self, imageFile):
+  def createThumb(self, imageFile, thumbSize):
     # landscape
     if imageFile.size[0] > imageFile.size[1]:
-      if sys.argv[3] < imageFile.size[1]:
+      if int(thumbSize) > imageFile.size[1]:
         if imageFile.format is 'JPEG':
           imageFile = imageFile.rotate(180, expand=True);
 
-      imageFile.thumbnail((int(sys.argv[3]), int(imageFile.size[1])), Image.ANTIALIAS);
+      imageFile.thumbnail((int(thumbSize), int(imageFile.size[1])), Image.ANTIALIAS);
     # portrait
     else:
-      imageFile.thumbnail((int(imageFile.size[0]), int(sys.argv[3])), Image.ANTIALIAS);
+      imageFile.thumbnail((int(imageFile.size[0]), int(thumbSize)), Image.ANTIALIAS);
+
+    return imageFile;
 
   # imageType can be image (by default) or the filename in case we are passing the folder
-  def saveFile(self, imageFile, imageType = 'image'):
-    if len(sys.argv) > 4:
-      if sys.argv[4] is not None: filename = '.thumb.' + sys.argv[4];
+  def saveFile(self, imageFile, filePath, customName, imageType = 'image'):
+    if customName is not None:
+      filename = '.thumb.' + customName;
     else:
       filename = '.thumb';
 
     if imageType == 'image':
-      imageFile.save(
-        os.path.splitext(sys.argv[2])[0] + filename + os.path.splitext(sys.argv[2])[1],
+      return imageFile.save(
+        os.path.splitext(filePath)[0] + filename + os.path.splitext(filePath)[1],
         imageFile.format,
         quality=100
       );
     else:
-      imageFile.save(
+      return imageFile.save(
         sys.argv[2] + os.path.splitext(imageType)[0] + filename + os.path.splitext(imageType)[1],
         imageFile.format,
         quality=100
--- ux.py
+++ ux.py
@@ -1,79 +1,119 @@
 #!/usr/bin/env python
 import wx
 
-import sys, os, Image
+import sys, os, Image, string
 import helpers
 
 class MainWindow(wx.Frame):
   def __init__(self, parent, title):
     # main window
-    wx.Frame.__init__(self, parent, title = title, size = (800, 600))
-    self.selectedPath = None;
-    self.InitUI()
+    wx.Frame.__init__(self, parent, title = title, size = (800, 600));
+    self.selectedPath = [];
+    self.InitUI();
     # render the frame
-    self.Center()
-    self.Show()
+    self.Center();
+    self.Show();
 
   def InitUI(self):
     # type folder / image
-    self.fileRadio = wx.RadioButton(self, label = 'Single file', pos = (20, 0))
-    self.fileRadio.SetValue(True)
-    self.Bind(wx.EVT_RADIOBUTTON, self.changeDialogTypeFile, self.fileRadio)
+    self.fileRadio = wx.RadioButton(self, label = 'Single file', pos = (20, 0));
+    self.fileRadio.SetValue(True);
+    self.Bind(wx.EVT_RADIOBUTTON, self.changeDialogTypeFile, self.fileRadio);
 
-    self.folderRadio = wx.RadioButton(self, label = 'Folder', pos = (120, 0))
-    self.Bind(wx.EVT_RADIOBUTTON, self.changeDialogTypeDir, self.folderRadio)
+    self.folderRadio = wx.RadioButton(self, label = 'Folder', pos = (120, 0));
+    self.Bind(wx.EVT_RADIOBUTTON, self.changeDialogTypeDir, self.folderRadio);
 
     # directory dialog
-    self.openDirBrowser = wx.Button(self, label = 'Search for file or folder', pos = (20, 30))
+    self.openDirBrowser = wx.Button(self, label = 'Search for file or folder', pos = (20, 30));
+    # open file dialog by default
+    self.Bind(wx.EVT_BUTTON, self.OpenFileDialog, self.openDirBrowser);
 
     # path control (input)
-    self.stringLabel = wx.StaticText(self, label = 'Append to end of file: ', pos = (20, 60))
-    self.stringTxt = wx.TextCtrl(self, pos = (300, 60))
+    self.stringLabel = wx.StaticText(self, label = 'Append to end of file: ', pos = (20, 60));
+    self.stringTxt = wx.TextCtrl(self, pos = (300, 60));
+
+    # size of thumb
+    self.sizeLabel = wx.StaticText(self, label = 'Size of thumb: ', pos = (20, 90));
+    self.sizeTxt = wx.TextCtrl(self, pos = (300, 90));
 
     # button
-    self.applyBtn = wx.Button(self, label = 'Create thumbs', pos = (500, 90))
-    self.Bind(wx.EVT_BUTTON, self.OnClick, self.applyBtn)
+    self.applyBtn = wx.Button(self, label = 'Create thumbs', pos = (500, 120));
+    self.Bind(wx.EVT_BUTTON, self.OnClick, self.applyBtn);
 
     # status text
-    self.console = wx.TextCtrl(self, style = wx.TE_MULTILINE | wx.TE_READONLY, size = (600, 300), pos = (20, 120))
+    self.console = wx.TextCtrl(self, style = wx.TE_MULTILINE | wx.TE_READONLY, size = (600, 300), pos = (20, 150));
 
   def changeDialogTypeFile(self, event):
-    self.Bind(wx.EVT_BUTTON, self.OpenFileDialog, self.openDirBrowser)
+    self.Bind(wx.EVT_BUTTON, self.OpenFileDialog, self.openDirBrowser);
 
   def changeDialogTypeDir(self, event):
-    self.Bind(wx.EVT_BUTTON, self.OpenDirDialog, self.openDirBrowser)
+    self.Bind(wx.EVT_BUTTON, self.OpenDirDialog, self.openDirBrowser);
 
   def OpenDirDialog(self, event):
+    self.selectedPath = [];
+
     dirDialog = wx.DirDialog(
       self,
       message = 'Choose a directory: ',
       defaultPath = os.getcwd(),
       style = wx.DD_DEFAULT_STYLE,
       pos = (60, 220)
-    )
+    );
 
     if dirDialog.ShowModal() == wx.ID_OK:
-      self.selectedPath = dirDialog.GetPath()
+      self.selectedPath.append(dirDialog.GetPath());
 
-    dirDialog.Destroy()
+    dirDialog.Destroy();
     return self.selectedPath;
 
   def OpenFileDialog(self, event):
+    self.selectedPath = [];
+
     fileDialog = wx.FileDialog(
       self,
       message = 'Choose a file: ',
       defaultDir = os.getcwd(),
       style = wx.OPEN | wx.MULTIPLE | wx.CHANGE_DIR
-    )
+    );
 
     if fileDialog.ShowModal() == wx.ID_OK:
-      paths = fileDialog.GetPaths()
+      paths = fileDialog.GetPaths();
+
       for path in paths:
-        print path
-    fileDialog.Destroy()
+        self.selectedPath.append(path);
+
+    fileDialog.Destroy();
+    return self.selectedPath;
 
   def OnClick(self, event):
-    print self.selectedPath;
+    imgHelper = helpers.ImageHelper();
+
+    if self.selectedPath:
+      # if more than one than they are files
+      # act accordingly
+      if len(self.selectedPath) > 1:
+        print len(self.selectedPath);
+      # if less than 2 than it can be file or folder
+      elif len(self.selectedPath) < 2:
+        pathParts = string.split(self.selectedPath[0], '/');
+        lastPathPart = pathParts[len(pathParts) - 1];
+        checkType = string.split(lastPathPart, '.');
+
+        # if it's a file
+        if len(checkType) > 1:
+          imageFile = Image.open(self.selectedPath[0]);
+          imageX = imageFile.size[0] / 2;
+          imageY = imageFile.size[1] / 2;
+
+          imgHelper.createThumb(imageFile, self.sizeTxt.GetValue());
+          imgHelper.saveFile(imageFile, self.selectedPath[0], self.stringTxt.GetValue());
+        # if it's a folder
+        else:
+          print 'folder';
+    # array is empty
+    else:
+      print 'Please select a file or folder.';
+
     # imgHelper = helpers.ImageHelper();
     # if len(sys.argv) > 1:
     #   # image file
@@ -110,9 +150,9 @@ class MainWindow(wx.Frame):
 
 # run the app
 def main():
-  app = wx.App(False)
-  frame = MainWindow(None, "GUI for thumb creation")
-  app.MainLoop()
+  app = wx.App(False);
+  frame = MainWindow(None, "GUI for thumb creation");
+  app.MainLoop();
 
 if __name__ == '__main__':
-  main()
+  main();
